e8c5e7db90e8a40d89b70664056e9b6d6d3407da
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-03-30 23:20:56 +0200

    preprocessor directive that includes the header file Bitmap

--- src/Bitmap.cpp
+++ src/Bitmap.cpp
@@ -1,7 +1,7 @@
 #include "Bitmap.h"
 
 namespace caveofprogramming {
-     Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height) {
+     Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height) , m_pPixels(new uint8_t[width*height*3]) {
         // Constructor implementation
     }
 
