666eca067f131ef5e85b9f44146bbc9d6983a3c4
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-04-04 07:40:46 +0200

    using the histagram

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<cstdint>
-#include"Bitmap.h"
+#include <iostream>
+#include <cstdint>
+#include "Bitmap.h"
 #include "Mandelbrot.h"
 
 using namespace caveofprogramming;
@@ -8,45 +8,69 @@ using namespace std;
 
 int main()
 {
-	int const WIDTH = 800;
-	int const HEIGHT = 600;
+    int const WIDTH = 800;
+    int const HEIGHT = 600;
 
-	Bitmap bitmap(WIDTH, HEIGHT);
-	Mandelbrot mandelbrot;
-	double min = 999999;
-	double max = -999999;
+    Bitmap bitmap(WIDTH, HEIGHT);
+    Mandelbrot mandelbrot;
+    double min = 999999;
+    double max = -999999;
 
-	unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS+1]{0});
+    unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS + 1] {0});
+    unique_ptr<int[]> fractal(new int[WIDTH * HEIGHT] {0});
 
-	//bitmap.setPixel(WIDTH / 2, 100, 255, 255, 255);
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            double xFractal = (x - WIDTH / 2 - 200) * 2.0 / HEIGHT;
+            double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
 
-	for (int y = 0; y < HEIGHT; y++) {
-		for (int x = 0; x < WIDTH; x++) {
-			//bitmap.setPixel(x, y, 0, 0, 255);// blue
-			//bitmap.setPixel(x, y, 255, 0, 0); // Set the color to red
-			double xFractal = (x - WIDTH / 2-200) * 2.0 / HEIGHT;
-			double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
+            int iterations = mandelbrot.getIterations(xFractal, yFractal);
+            fractal[y * WIDTH + x] = iterations;
 
-			int iterations = mandelbrot.getIterations(xFractal, yFractal);
-			histogram[iterations]++;
+            histogram[iterations]++;
+        }
+    }
 
-			uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+    cout << "Histogram: " << endl;
 
-			//color = (uint8_t)(pow(color / 255.0, 3) * 255);
+    int maxCount = 0;
+    for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++) {
+        cout << histogram[i] << ", " << flush;
+        maxCount += histogram[i];
+    }
 
-			color = color * color * color;
+    cout << min << ", " << max << endl;
 
-			bitmap.setPixel(x, y, color, 0, 0);
+    int total = 0;
+    for (int i = 0; i < Mandelbrot::MAX_ITERATIONS; i++) {
+        total += histogram[i];
+    }
 
-			if (xFractal < min) min = xFractal;
-			if (xFractal > max) max = xFractal;
-		}
-	}
-	//bitmap.setPixel(100, 100, 255, 0, 0); // Set the color to red
-	cout << min << ", " << max << endl;
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            int iterations = fractal[y * WIDTH + x];
 
-	bitmap.write("mbarek2.bmp");
-	std::cout << "Finished" << std::endl;
+            uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
 
-	return 0;
+            double hue = 0.0;
+
+            for (int i = 0; i < iterations; i++) {
+				hue += (double)histogram[i] / total;
+           }
+            
+            
+           // color = color * color * color;
+            uint8_t red = red = 0;
+            uint8_t green = hue * hue*255;
+            uint8_t blue = blue = 0;
+
+            bitmap.setPixel(x, y, red, green, blue);
+
+        }
+    }
+
+    bitmap.write("mbarek6.bmp");
+    std::cout << "Finished" << std::endl;
+
+    return 0;
 }
